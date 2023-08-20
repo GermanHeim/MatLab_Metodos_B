41f7bcdc5ca8ccbd12df0ef5f435e2b1a20a0c3f
Germán Heim <77305706+GermanHeim@users.noreply.github.com>
2023-08-20 11:32:31 -0300

    Update Problema4.m

--- Guia 1/Problema4.m
+++ Guia 1/Problema4.m
@@ -11,9 +11,14 @@ suma2=0;
 for i=1:n
     suma1 = chop(suma1+1/i^2, 3);
 end
-disp(suma1)
+disp(suma1) % 1.59
 
 for i=n:-1:1
     suma2=chop(suma2+1/i^2, 3);
 end
-disp(suma2)
+disp(suma2) % 1.64
+
+% Sumando de n=1 a 10000, se suman primero los numeros mas pequeños, lo que
+% hace que el redondeo sea mayor a cuando se suman primero los grandes y
+% luego los pequeños (usando computación limitada como 3 cifras
+% significativas).

f5ed100c7755d8fa2853372ff2c0a9982088d51c
Germán Heim <77305706+GermanHeim@users.noreply.github.com>
2023-09-12 14:55:20 -0300

    Guia 5
    Cuestionable los metodos para resolver esto, full manual, pero bueno, lo quieren asi

--- Guia 5/Problema2.m
+++ Guia 5/Problema2.m
@@ -3,11 +3,29 @@
 clear all
 close all
 clc
+format long
+
+x0=[0; 0];
+
+syms X1 X2
+Fs=[(X1 - 1)^2 + (X2 - 1)^2 - 1; X1-X2];
+Js=jacobian(Fs);
+
+F=matlabFunction(Fs);
+J=matlabFunction(Js);
+F=@(X) F(X(1), X(2)); % Para poder reemplazar con un vector x0
+J=@(X) J(X(1), X(2));
+
+hold on
+f1 = ezplot((X1 - 1)^2 + (X2 - 1)^2 - 1, [-1 , 2.5]);
+set(f1, 'Color', 'red')
+f2 = ezplot(X1-X2, [-1 , 2.5]);
+set(f2, 'Color', 'blue')
 
 
-f1=@(x1, x2) x2^2 -2*x2 + 1 -x1;
-f2=@(x1, x2) sqrt(x1) + x2 -5;
+newton_raphson_multivariable(F, J, x0, 0.00001, 100)
 
-% f1=@(x1, x2) (x1 - 1)^2 + (x2 - 1)^2;
-% f2=@(x1, x2) x1 - x2;
-F = {f1, f2}; % Array de celdas
+% Al utilizar [1; 1] como x0 el J que es [2(x1−1),​ 2(x2−1); 1, −1] se
+% convierte en singular (arriba es 0, 0). Es por esto que no se puede
+% resolver linsolve(J, -F), es decir no sirven aquellos valores de x1=x2=1,
+% ya que hacen que J sea singular
---
+++ Guia 5/newton_raphson_multivariable.m
@@ -0,0 +1,31 @@
+function output = newton_raphson_multivariable(F, J, x0, tol, N)
+%NEWTON_RHAPSON_MULTIVARIABLE Metodo de Newton Rhapson Multivariable
+%  F es una matriz funcion handle de una variable X la igual que J, x0 (vect) un vector de puntos
+%  iniciales, tol una tolerancia (int), y N un numero maximo de iteraciones
+%  (int)
+F_eval=[];
+J_eval=[];
+iteracion = 0;
+
+if size(x0, 2) ~= 1 % Vector columna
+    x0=transpose(x0);
+end
+
+while iteracion <= N
+    iteracion=iteracion+1;
+    F_eval = F(x0);
+    J_eval = J(x0);
+    ro = linsolve(J_eval, -F_eval);
+
+    x0=ro+x0;
+
+    if norm(F_eval)<=tol
+        disp('Raices encontradas');
+        disp(x0);
+        disp(['Iteración: ', num2str(iteracion)])
+        return
+    end
+        
+end
+output=x0;
+
